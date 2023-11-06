2a1a0bafb7abc67fba394b4ca0530a93a1a2d512
githubbeginnernocap <148562883+githubbeginnernocap@users.noreply.github.com>
2023-11-06 16:41:20 +0300

    Add files via upload

---
+++ ПР3 Задание 3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class SQLCommand
+{
+    private string commandText;
+
+    public string CommandText
+    {
+        get { return commandText; }
+        set { commandText = value.ToUpper(); }
+    }
+
+    public SQLCommand(string text)
+    {
+        CommandText = text;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SQLCommand sqlCommand = new SQLCommand("select * from customers");
+
+        Console.WriteLine("SQL Query: " + sqlCommand.CommandText);
+    }
+}
